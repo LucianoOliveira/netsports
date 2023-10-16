f23a66fefd456cb41c3af33ad152bd3289d92ae6
Luciano Oliveira <luciano8.oliveira@gmail.com>
2023-10-16 17:58:50 +0100

    full send it

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,21 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! Welcome to NetSports! Uploaded from GitHub</p>"
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World! Welcome to NetSports! Uploaded from GitHub</p>"
 
-@app.route("/test")
-def test():
-    return "<p>Testing New Route</p>"
+# @app.route("/test")
+# def test():
+#     return "<p>Testing New Route</p>"
 
-@app.route("/new")
-def new():
-    return "<p>New Route from VsCode</p>"
+# @app.route("/new")
+# def new():
+#     return "<p>New Route from VsCode</p>"
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True) 
