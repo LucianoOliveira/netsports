f538f53517fa36a52411073d655f6837c64e3205
LucianoOliveira <luciano8.oliveira@gmail.com>
2023-10-16 16:30:08 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World! Welcome to NetSports! Uploaded from GitHub</p>"
+
+@app.route("/test")
+def test():
+    return "<p>Testing New Route</p>"
